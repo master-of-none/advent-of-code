5fcadefe7c59bb3e434b736ef407e4929721699e
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2023-12-03 14:03:40 -0800

    Refactor code to use a class and add puzzle_1 method

--- day1/code.py
+++ day1/code.py
@@ -1,13 +1,22 @@
 import sys
-file = open(sys.argv[1]).read().strip()
-result = 0
-
-for line in file.split('\n'):
-    digits = []
-    for c in line:
-        if c.isdigit():
-            digits.append(c)
-    temp = int(digits[0]+digits[-1])
-    result += temp
-print(result)
+class Puzzle:
+    def puzzle_1(self, file):
+        result = 0
+        for line in file.split('\n'):
+            digits = []
     
+            for c in line:
+                if c.isdigit():
+                    digits.append(c)
+            
+            temp = int(digits[0]+digits[-1])
+            result += temp
+        
+        return result
+
+if __name__ == "__main__":
+    file = open(sys.argv[1]).read().strip()
+    puzzle = Puzzle()
+    
+    result = puzzle.puzzle_1(file)
+    print(f"The puzzle 1 value is: {result}")
