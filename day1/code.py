92ccc657f6771e20728a6f86998d23fa19d2afaa
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2023-12-03 17:33:57 -0800

    Refactor puzzle_2 method

--- day1/code.py
+++ day1/code.py
@@ -16,31 +16,26 @@ class Puzzle:
 
     def puzzle_2(self, file):
         p1 = 0
-        p2 = 0
         
         for line in file.split('\n'):
             p1_digits = []
-            p2_digits = []
-            
             for i, c in enumerate(line):
                 if c.isdigit():
                     p1_digits.append(c)
-                    p2_digits.append(c)
                     
                 for d, val in enumerate(['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']):
                     if line[i:].startswith(val):
-                        p2_digits.append(str(d+1))
+                        p1_digits.append(str(d+1))
                 
             p1 += int(p1_digits[0] + p1_digits[-1])
-            p2 += int(p2_digits[0] + p2_digits[-1])
             
-        return p1, p2 
+        return p1
         
 if __name__ == "__main__":
     file = open(sys.argv[1]).read().strip()
     puzzle = Puzzle()
     
-    result = puzzle.puzzle_1(file)
-    print(f"The puzzle 1 value is: {result}")
-    result1, result2 = puzzle.puzzle_2(file)
-    print(f"The puzzle 2 value is: {result1} {result2}")
+    result1 = puzzle.puzzle_1(file)
+    print(f"The puzzle 1 value is: {result1}")
+    result2 = puzzle.puzzle_2(file)
+    print(f"The puzzle 2 value is: {result2}")
